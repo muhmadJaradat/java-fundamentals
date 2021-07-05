e13c36633ece443e8b8a6b60a2d7431cc7e1637a
muhmadjaradat <muhmadjradat@gmail.com>
2021-07-05 17:47:20 +0300

    adding lab06

---
+++ inhertiance/lib/src/test/java/inhertiance/ReviewTest.java
@@ -0,0 +1,18 @@
+package inhertiance;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class ReviewTest {
+
+//        @Test void ReveiwAuthorName(){
+//            Review newReview=new Review("Mohammad","This Resturant has great food!",4);
+//            assertEquals(newReview.getAuthor(),"Mohammad");
+//        }
+//        @Test void ReviewToString(){
+//            Review newReview=new Review("Mohammad","This Resturant has great food!",4);
+//            assertEquals(newReview.toString(),"Author name: Mohammad\nReview body: This Resturant has great food!\nGiven stars: 4");
+//
+//        }
+
+}
